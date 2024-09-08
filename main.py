9f3c56b34d40adfa42df9b63b41c54105c948a4f
Arual1o <laurae.vargas@udea.edu.co>
2024-09-08 14:43:17 -0500

    avance proyecto

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import mysql.connector
 import csv
 import os
 import json
+from clases.recursos import recurso
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   database="pis_iiidb"
 )
-c = mydb.cursor()
-c.execute("SHOW DATABASES")
-for i in c:
-    print(i)
-c = mydb.cursor()
-mydb.close()
+# c = mydb.cursor()
+# c.execute("SHOW DATABASES")
+# for i in c:
+#     print(i)
+# c = mydb.cursor()
+# mydb.close()
 
 def print_opcion():
     print({"GESTOR DE CONTENIDO\n",          
@@ -23,7 +24,22 @@ def print_opcion():
            "4. Eliminar recurso\n"
            "5. Salir"
            })
-
+def crear_recurso(recurso_id):
+    print("Crear recurso \n")
+    #fecha=int()
+    tipo_recurso=input("Ingrese el tipo de recurso: \n")
+    recurso_nuevo = {
+        #'id': str(recurso_id),
+        'Tipo de recurso':tipo_recurso,
+        # 'Descripcion': nombre,
+        # 'Imagen': apellido,
+        # 'Recurso ': recurso
+        # 'Link':
+        # 'Fecha': str(fecha)
+    }
+    
+    mydb.info.insert_one(recurso_nuevo)
+    print("Recurso creado exitosamente")
 def ingresado():
     print_opcion()
     opcion=input("Ingrese acción a realizar: ")
