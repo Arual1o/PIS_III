65d018f4994240fcc42b8e3a5be0c99c5eb1430e
Anaseguro <142312442+Anaseguro@users.noreply.github.com>
2024-10-03 17:27:28 -0500

    Add files via upload

--- Frontend/app/layout.js
+++ Frontend/app/layout.js
@@ -1,1 +1,17 @@
+import { Inter } from "next/font/google";
+import "../styles/main.scss";
 
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Gestor de contenido HAMA",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
